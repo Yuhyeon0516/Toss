840f3a5aac429eccbdc424a4c47accff03e394d3
Yuhyeon Kim <120432007+Yuhyeon0516@users.noreply.github.com>
2023-01-07 16:11:28 +0900

    Add files via upload

---
+++ StockRank-SwiftUI-Navigation/StockRank-SwiftUI/StockDetailView.swift
@@ -0,0 +1,33 @@
+//
+//  StockDetailView.swift
+//  StockRank-SwiftUI
+//
+//  Created by Yuhyeon Kim on 2023/01/07.
+//
+
+import SwiftUI
+
+struct StockDetailView: View {
+    @Binding var stock: StockModel
+    
+    var body: some View {
+        VStack(spacing: 40) {
+            Image(stock.imageName)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 120, height: 120)
+            Text(stock.name)
+                .font(.system(size: 30, weight: .bold, design: .default))
+            Text("\(stock.price) 원")
+                .font(.system(size: 20, weight: .bold, design: .default))
+                .foregroundColor(stock.diff > 0 ? .red:.blue)
+        }
+    }
+}
+
+struct StockDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        StockDetailView(stock: .constant(StockModel.list[0]))
+            .preferredColorScheme(.dark)
+    }
+}
---
+++ StockRank-SwiftUI-Navigation/StockRank-SwiftUI/StockRankView.swift
@@ -0,0 +1,38 @@
+//
+//  ContentView.swift
+//  StockRank-SwiftUI
+//
+//  Created by Yuhyeon Kim on 2023/01/07.
+//
+
+import SwiftUI
+
+struct StockRankView: View {
+    @State var list = StockModel.list
+    
+    var body: some View {
+        NavigationView {
+            List($list) { $item in
+                ZStack {
+                    NavigationLink {
+                        StockDetailView(stock: $item)
+                    } label: {
+                        EmptyView()
+                    }
+                    StockRankRow(stock: $item)
+                }
+                .listRowInsets(EdgeInsets())
+                .frame(height: 80)
+            }
+            .listStyle(.plain)
+            .navigationTitle("Stock Rank")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        StockRankView()
+            .preferredColorScheme(.dark)
+    }
+}
